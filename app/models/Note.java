398827cbb73ead48a8be3313fb19b54f0b9db3aa
Erik Larkö <erik.larko@purplescout.se>
2014-02-21 22:49:03 +0000

    Fixed wrong datatype in Note

--- app/models/Note.java
+++ app/models/Note.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.Column;
+import javax.persistence.Lob;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
