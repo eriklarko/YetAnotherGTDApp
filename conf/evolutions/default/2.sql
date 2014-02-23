ac0a91550a571b2ec321ce666577296133a41b79
Erik Larkö <erik.larko@purplescout.se>
2014-02-23 23:37:05 +0100

    Another attempt at evol

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,3 +1,5 @@
+# --- !Ups
+
 create table user (
   id                        bigint not null,
   email                     varchar(255),
@@ -26,3 +28,20 @@ alter table note add constraint fk_note_owner_2 foreign key (owner_id) reference
 create index ix_note_owner_2 on note (owner_id);
 alter table tag add constraint fk_tag_owner_3 foreign key (owner_id) references user (id) on delete restrict on update restrict;
 create index ix_tag_owner_3 on tag (owner_id);
+
+
+# --- !Downs
+
+drop index if exists ix_filter_owner_1
+drop index if exists ix_note_owner_2
+drop index if exists ix_tag_owner_3
+
+alter table filter drop column owner_id;
+alter table filter drop column starred;
+alter table note drop column owner_id;
+alter table tag drop column owner_id;
+
+drop table if exists user;
+drop sequence if exists user_seq;
+
+
