cc16c09d666195ec780aa624ded1b07f4c752073
Erik Larkö <erik@larko.se>
2015-06-16 21:07:46 +0200

    Print error if the server cannot start

--- TaggyGo/src/main.go
+++ TaggyGo/src/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"webapp"
+	"log"
 )
 
 func main() {
 	r := gin.Default()
 	setupRoutes(r)
 
-	r.RunTLS(":8080", "keys/dev.crt", "keys/dev.key") // listen and serve https on 0.0.0.0:8080
+	log.Println("Starting the server")
+	err := r.RunTLS(":8080", "keys/dev.crt", "keys/dev.key") // listen and serve https on 0.0.0.0:8080
+	if err != nil {
+		log.Fatalf("Unable to start the server. %v\n", err)
+	}
 }
 
 func setupRoutes(r *gin.Engine) {
