13ca8e910c4148a5f5a9ae27b836f032a45a4b52
Erik Larkö <erik@larko.se>
2015-06-16 23:07:20 +0200

    More over engineered environment handling

--- TaggyGo/src/main.go
+++ TaggyGo/src/main.go
@@ -6,21 +6,26 @@ import (
 	"log"
 	"os"
 	"github.com/rcrowley/go-metrics"
+	"strconv"
 )
+
+var port uint16 = 8080
+var certificateFile string
+var keyFile string
 
 func main() {
 	env := os.Getenv("TAGGY_ENVIRONMENT")
 	if env == "prod" {
 		setupProductionEnvironment();
 	} else {
-		log.Println("[WARN] Starting Taggy in develoment environment")
+		log.Println("[WARN] Starting Taggy in develoment environment. Start it in production with 'export TAGGY_ENVIRONMENT=prod'")
 		setupDevEnvironment()
 	}
 
 	ginEngine := gin.Default()
 	ginEngine.Use(goMetricsMiddleWare)
 	setupRoutes(ginEngine)
-	startTheServer(ginEngine)
+	startTheServerAndBlock(ginEngine)
 }
 
 // Measures the time each request takes and how often each endpoint is accessed.
@@ -33,10 +38,37 @@ func goMetricsMiddleWare(c *gin.Context) {
 
 func setupProductionEnvironment() {
 	gin.SetMode(gin.ReleaseMode)
+	certificateFile = os.Getenv("TAGGY_CERTFILE")
+	keyFile = os.Getenv("TAGGY_KEYFILE")
+
+	if len(certificateFile) == 0 || len(keyFile) == 0 {
+		log.Fatalln("You must set TAGGY_CERTFILE and TAGGY_KEYFILE environment variables to point to your HTTPS certificate files")
+	}
+
+	readPortFromEnvIfItsSet()
 }
 
 func setupDevEnvironment() {
 	gin.SetMode(gin.DebugMode)
+	certificateFile = "keys/dev.crt"
+	keyFile = "keys/dev.key"
+	readPortFromEnvIfItsSet()
+}
+
+func readPortFromEnvIfItsSet() {
+	rawPort := os.Getenv("TAGGY_PORT")
+	if len(rawPort) == 0 {
+		return
+	}
+
+	uncheckedPort, err := strconv.Atoi(rawPort)
+	if err != nil {
+		log.Fatalf("Unknown port set by environment variable TAGGY_PORT: '%s'\n", rawPort)
+	} else if uncheckedPort >= 0 && uncheckedPort < 65536 {
+		port = uint16(uncheckedPort)
+	} else {
+		log.Fatalf("Illegal port set by environment variable TAGGY_PORT: %d. Must be >= 0 and < 65536\n", uncheckedPort)
+	}
 }
 
 func setupRoutes(r *gin.Engine) {
@@ -57,9 +89,11 @@ func setupNoteRoutes(r *gin.Engine) {
 	r.DELETE("/notes/:id", webapp.DeleteNote)
 }
 
-func startTheServer(r *gin.Engine) {
-	log.Println("Starting the server")
-	err := r.RunTLS(":8080", "keys/dev.crt", "keys/dev.key") // listen and serve https on 0.0.0.0:8080
+func startTheServerAndBlock(r *gin.Engine) {
+	portAsString := strconv.Itoa(int(port))
+
+	log.Printf("Starting the server on port %s using %s and %s\n", portAsString, certificateFile, keyFile)
+	err := r.RunTLS(":" + portAsString, certificateFile, keyFile) // listen and serve https on localhost
 	if err != nil {
 		log.Fatalf("Unable to start the server. %v\n", err)
 	}
