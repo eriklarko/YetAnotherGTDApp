aee04b2cca8d12f4615400fae910ea90c77eeac5
Erik Larkö <erik@larko.se>
2015-06-16 21:01:20 +0200

    Small update to README and helper scripts

--- TaggyGo/setup-gopath.sh
+++ TaggyGo/setup-gopath.sh
@@ -1,3 +1,8 @@
+if [ ! -d "src" ]; then
+  echo "You don't seem to be standing at root of the TaggyGo project. It should contain a folder named src";
+  exit 1;
+fi
+
 PWD=`pwd`
 DEPS_PATH="${PWD}"
 echo "export GOPATH=$DEPS_PATH"
