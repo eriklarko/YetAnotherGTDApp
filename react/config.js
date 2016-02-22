38bb694cd12582c0b4a50437316536d378f9d33a
Erik Larkö <erik@larko.se>
2016-02-22 21:26:55 +0100

    removed remarkable by jonschlinkert because it sucks

--- react/config.js
+++ react/config.js
@@ -175,3 +175,4 @@ System.config({
     }
   }
 });
+
--- react/src/components/payload-edit/rich-text.tsx
+++ react/src/components/payload-edit/rich-text.tsx
@@ -1,6 +1,5 @@
 import {Component, ReactElement} from 'react';
 import {Note} from "../../models/note-model";
-import * as Remarkable from "jonschlinkert/remarkable";
 import {payloadActionCreator} from "../../actions/payload-action-creator";
 import {Tags} from "../tags";
 
@@ -39,10 +38,6 @@ export class RichTextPayload extends Component<Props, State> {
   }
 
   private render() : ReactElement<any> {
-    let md = new Remarkable();
-    let unsafeHtml = { __html: md.render(this.state.unsavedPayload) };
-    let renderedPayload = <div dangerouslySetInnerHTML={unsafeHtml} />
-
     let editablePayload = <textarea ref="ta" onChange={this.onTextAreaChanged} value={this.state.unsavedPayload} style={{width: "100%", height: "100%"}} />
 
     let hasUnsavedChanges = this.props.note.payload !== this.state.unsavedPayload;
@@ -60,7 +55,7 @@ export class RichTextPayload extends Component<Props, State> {
                 <Tags note={this.props.note} style={{float: "left"}}/>
             </div>
         </div>
-        <div className="col-md-6">{renderedPayload}</div>
+        <div className="col-md-6">TODO, remarkable from jonschlinkert did not work out</div>
       </div>
     );
   }
