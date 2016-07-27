59a9df8508fda27302df523b6833b7668372d430
Erik Larkö <erik.larko@safersociety.com>
2016-07-27 16:42:43 +0200

    Router now works

--- react-simple/src/index.tsx
+++ react-simple/src/index.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Router, Route, hashHistory } from "react-router";
 
 import { Hello } from "./components/Hello";
 
+
+const App = () => <Hello compiler="TypeScript" framework="React" />
+const Bpp = () => <Hello compiler="SHITNE" framework="React" />
+const route = (<Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="/a" component={Bpp} />
+</Router>);
+
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    route,
     document.getElementById("example")
 );
